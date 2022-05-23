ab12834c9fdfd444a92efb9b02147ea0f548e419
João Martins <joaofmartins@ua.pt>
2022-05-23 16:32:48 +0100

    bottle http server to control app parameters.

--- RPi4/App_RPi/src/main_app/server.py
+++ RPi4/App_RPi/src/main_app/server.py
@@ -1,13 +1,24 @@
 from bottle import Bottle, run
 from env import Env
 
+"""Small http server as to provide easy to use endpoints for configuration and RW operations.
+Meant for internal use due to not implmenting any security features. 
+An external server may be used as proxy. 
+"""
+
 app = Bottle()
 
 @app.get('/t_temp')
 def get_t_temp():
+    """
+    Get current threshold temperature.
+    """
     return str(Env.TEMP_THRESHOLD)
 
 @app.post('/t_temp/<temp:int>')
 def theshold_temp(temp:int):
+    """
+    Set threshold temperature.
+    """
     print("@theshold_temp: temp value: "+str(temp))
     Env.TEMP_THRESHOLD = temp
