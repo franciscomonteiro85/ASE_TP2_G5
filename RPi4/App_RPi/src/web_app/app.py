efdf5cb7896db2741de51c60a1c2fbb7110fcfd2
Francisco <francisco.monteiro@ua.pt>
2022-05-23 18:40:45 +0100

    Added connection to webserver

--- RPi4/App_RPi/src/web_app/app.py
+++ RPi4/App_RPi/src/web_app/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route('/temperature/<temperature>')
-def index(temperature):
-    showTemperature()
-    return render_template('index.html', temperature=temperature)
+@app.route('/temperature')
+def index():
+    return render_template('index.html')
 
-def showTemperature():
-    return 0
+@app.route('/get_temperature')
+def get_temperature():
+    temperature = requests.get("localhost:8080/temp")
+    return make_response({"output": int(temperature.content)})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
