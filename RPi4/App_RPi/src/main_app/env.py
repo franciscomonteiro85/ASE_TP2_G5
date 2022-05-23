9876f43e37b057df35b0b247bcb97147629647b3
João Martins <joaofmartins@ua.pt>
2022-05-23 16:10:33 +0100

    update env to be a class with static atributes.

--- RPi4/App_RPi/src/main_app/env.py
+++ RPi4/App_RPi/src/main_app/env.py
@@ -1,6 +1,14 @@
-GPIO_LED_NR = 37
-PWM_LED_NR = 38
-PWM_LED_FREQ = 1000
+class Env:
+    
+    GPIO_LED_NR = 37
+    PWM_LED_NR = 38
+    PWM_LED_FREQ = 1000
 
-I2C_BUS = 1
-TC74_I2C_ADD = 0x4D
+    I2C_BUS = 1
+    TC74_I2C_ADD = 0x4D
+
+    E2PROM_CE_NR = 0
+    E2PROM_SPI_CH = 0
+
+    TEMP_THRESHOLD = 20
+
