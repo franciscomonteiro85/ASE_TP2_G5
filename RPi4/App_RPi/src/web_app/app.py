6c779655a6c2309e42aae3b0d61a4e7eaf54c36b
Francisco <francisco.monteiro@ua.pt>
2022-05-24 11:16:36 +0100

    Fixed minor route bug

--- RPi4/App_RPi/src/web_app/app.py
+++ RPi4/App_RPi/src/web_app/app.py
@@ -18,16 +18,14 @@ def set_threshold(threshValue):
     return threshValue
 
 @app.route('/set_duty_cycle/<stepValue>', methods=['POST'])
-def set_duty_cycle(duty_step):
-    requests.post("http://localhost:8080/t_duty/" + duty_step)
-    return duty_step
+def set_duty_cycle(stepValue):
+    requests.post("http://localhost:8080/t_step/" + stepValue)
+    return stepValue
 
-@app.route('/set_duty_cycle/<start_temp>', methods=['POST'])
+@app.route('/set_start_temp/<start_temp>', methods=['POST'])
 def set_start_temp(start_temp):
     requests.post("http://localhost:8080/t_start_temp/" + start_temp)
     return start_temp
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
